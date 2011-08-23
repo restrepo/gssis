3eaea529adbb4b79b6efc42dda05c60eb55323c5
root <root@tuxito>
2011-08-23 15:42:40 -0400

    Working in articles jet, done all less groups

---
+++ application/controllers/ArticlesController.php
@@ -0,0 +1,101 @@
+<?php
+
+class ArticlesController extends Zend_Controller_Action
+{
+
+    public $csvFile = null;
+
+    public $form = null;
+
+    public function init()
+    {
+        /* Initialize action controller here */
+      $this->form = new Zend_Form;	
+    }
+    private function getCsvButton($url)
+    {
+     $formb = new Zend_Form;
+     $formb->setAction("/gds/public/index/downloadcsv")->setMethod('post');
+     $formb->addElement('submit', 'csv', array('label' => 'Descargar csv'));
+     return $formb;
+    }
+
+    private function getForm()
+    {
+//       $form = new Zend_Form;
+      $this->form->setAction("/gds/public/articles/search")->setMethod('post');
+      $this->form->addElement('text', 'article', array('label' => 'Articulo'));
+      $this->form->addElement('text', 'autor', array('label' => 'Autor'));
+      $this->form->addElement('hidden', 'yearLabel', array('label' => 'Años (Intervalo)'));
+      $this->form->addElement('text', 'yearInit',array('disableLoadDefaultDecorators' => true,'decorators'=>Array('ViewHelper','Errors')));
+//       $this->form->yearInit->addValidator('Regex', false, array('/^[0-9]{4,}$/i'));
+      $this->form->addElement('text', 'yearEnd', array('disableLoadDefaultDecorators' => true,'decorators'=>Array('ViewHelper')));
+      $this->form->addElement('text', 'journal', array('label' => 'Revista'));
+      $this->form->addElement('text', 'group', array('label' => 'Grupo'));
+      $this->form->addElement('select','type',array('label'=>'Tipo de Artículo','value'=>'Todos','autocomplete'=>false,'multiOptions'=>array(''=>'Todos','(D+contains+\'Internacional\')+and+'=>'Internacionales',
+	      '(D+contains+\'Nacional\')+and+'  => 'Nacionales')));
+
+      $this->form->addElement('submit', 'search', array('label' => 'Buscar'));
+      return $this->form;
+    }
+
+    public function indexAction()
+    {
+        // action body
+     $this->view->form = $this->getForm();
+      echo $this->view->form;
+    }
+
+    public function searchAction()
+    {
+               if (!$this->getRequest()->isPost()) {
+            return $this->_forward('index');
+        }
+        $form = $this->getForm();
+        if (!$this->form->isValid($_POST)) {
+            // Failed validation; redisplay form
+            $this->view->form = $form;
+            return $this->render('form');
+        }
+
+        $values = $form->getValues();
+	
+	
+	$yearInitCode='';
+	$yearEndCode='';
+	if(!empty($values['yearInit']))
+	{
+	  $yearInitCode="(C>=".$values['yearInit'].")+and+";
+	}
+	if(!empty($values['yearEnd']))
+	{
+	  $yearEndCode="(C<=".$values['yearEnd'].")+and+";
+
+	}
+// 	  "(C+contains+'".$values['yearInit']."')+and+".
+// 	print_r($values);
+	$this->view->resultscsv = "<iframe style=\"height:100%;width:100%\"  src=\"".
+        "https://spreadsheets.google.com/tq?tqx=out:csv&tq=select+E,F,D,G,H,C,J,M+where+".
+        "(E+contains+'".$values['autor']."')+and+".
+        "(F+contains+'".$values['journal']."')+and+";
+	$this->view->resultscsv."(J+contains+'".$values['article']."')+order+by+A+desc&key=0AjqGPI5Q_Ez6dDA3ajhtYVVDOWdBckVhWm1MSFRET1E\"> Downloading</iframe>";
+	$this->view->results = "<iframe style=\"height:100%;width:100%\"  src=\"".
+	 "https://spreadsheets.google.com/tq?tqx=out:html&tq=select+E,F,D,G,H,C,J,M+where+".
+	  "(E+contains+'".$values['autor']."')+and+".
+	  "(F+contains+'".$values['journal']."')+and+".
+	  $yearInitCode.$yearEndCode.$values['type'].  
+	  "(J+contains+'".$values['article']."')+order+by+A+desc&key=0AjqGPI5Q_Ez6dDA3ajhtYVVDOWdBckVhWm1MSFRET1E\"> Searching</iframe>";
+	echo $this->getForm();
+	echo $this->getCsvButton("https://spreadsheets.google.com/tq?tqx=out:csv&tq=select+E,F,G,H,C,J+where+".
+	  "(E+contains+'".$values['autor']."')+and+".
+	  $yearInitCode.$yearEndCode.$values['type'].
+	  "(J+contains+'".$values['article']."')+order+by+A+desc&key=0AjqGPI5Q_Ez6dDA3ajhtYVVDOWdBckVhWm1MSFRET1E");
+	echo $this->view->results;
+// 	return $this->_forward('index');
+    }
+
+
+}
+
+
+
--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -2,42 +2,44 @@
 
 class IndexController extends Zend_Controller_Action
 {
-    var $csvFile;
+
+    public $csvFile = null;
+
+    public $form = null;
+
     public function init()
     {
         /* Initialize action controller here */
-	
+      $this->form = new Zend_Form;	
     }
 
     private function getForm()
     {
-      $form = new Zend_Form;
 //       $form = new Zend_Form;
-      $form->setAction("/gds/public/index/search")->setMethod('post');
-      $form->addElement('text', 'article', array('label' => 'Articulo'));
-      $form->addElement('text', 'autor', array('label' => 'Autor'));
-      $form->addElement('text', 'year', array('label' => 'Año'));
-      $form->addElement('text', 'journal', array('label' => 'Revista'));
-      $form->addElement('submit', 'search', array('label' => 'Buscar'));
-      return $form;
+      $this->form->setAction("/gds/public/index/search")->setMethod('post');
+      $this->form->addElement('text', 'article', array('label' => 'Articulo'));
+      $this->form->addElement('text', 'autor', array('label' => 'Autor'));
+      $this->form->addElement('text', 'yearInit', array('label' => 'Año Inicial','text', 'yearEnd', array('label' => 'Año Final')));
+//       $this->form->addElement();
+      $this->form->addElement('text', 'journal', array('label' => 'Revista'));
+      $this->form->addElement('text', 'group', array('label' => 'Grupo'));
+      $this->form->addElement('submit', 'search', array('label' => 'Buscar'));
+      return $this->form;
     }
 
-    private function getCsvButton($code)
+    private function getCsvButton($url)
     {
-      $form = new Zend_Form;
-      $form->setAction("/gds/public/index/downloadcsv")->setMethod('post');
-//       $form->addElement('submit', 'csv', array('label' => 'Descargar csv'));
-      $form->addElement('submit', 'csv', array('label' => 'Descargar csv'));
-//       $form->code=$code;
-      $this->view->resultscsv=$code;
-      return $form;
+     $formb = new Zend_Form;
+     $formb->setAction("/gds/public/index/downloadcsv")->setMethod('post');
+//       $formb->setAction($url);
+      $formb->addElement('submit', 'csv', array('label' => 'Descargar csv'));
+      return $formb;
     }
 
     public function indexAction()
     {
         // action body
-      $this->view->form = $this->getForm();;
-      echo $this->view->form;
+ 
     }
 
     public function searchAction()
@@ -46,7 +48,7 @@ class IndexController extends Zend_Controller_Action
             return $this->_forward('index');
         }
         $form = $this->getForm();
-        if (!$form->isValid($_POST)) {
+        if (!$this->form->isValid($_POST)) {
             // Failed validation; redisplay form
             $this->view->form = $form;
             return $this->render('form');
@@ -66,10 +68,15 @@ class IndexController extends Zend_Controller_Action
 	  "(C+contains+'".$values['year']."')+and+".
 	  "(J+contains+'".$values['article']."')+order+by+A+desc&key=0AjqGPI5Q_Ez6dDA3ajhtYVVDOWdBckVhWm1MSFRET1E\"> Searching</iframe>";
 // 	print_r($values);
+// 	$form->addElement('hidden', 'csvcode', array('value' => $this->view->resultscsv));
 	echo $this->getForm();
 // 	print_r($values);
 	echo "<!--<iframe style=\"height:100%;width:100%\"  src=\"https://spreadsheets.google.com/tq?tqx=out:html&tq=select+E,F,G,H,C,J+where+(F+contains+".$values['autor'].")+order+by+A+desc&key=0AjqGPI5Q_Ez6dDA3ajhtYVVDOWdBckVhWm1MSFRET1E\"> Searching</iframe>-->";
-	echo $this->getCsvButton($this->view->resultscsv);
+	echo $this->getCsvButton("https://spreadsheets.google.com/tq?tqx=out:csv&tq=select+E,F,G,H,C,J+where+".
+	  "(E+contains+'".$values['autor']."')+and+".
+	  "(F+contains+'".$values['journal']."')+and+".
+	  "(C+contains+'".$values['year']."')+and+".
+	  "(J+contains+'".$values['article']."')+order+by+A+desc&key=0AjqGPI5Q_Ez6dDA3ajhtYVVDOWdBckVhWm1MSFRET1E");
 	echo $this->view->results;
 // 	return $this->_forward('index');
 	
@@ -87,12 +94,29 @@ class IndexController extends Zend_Controller_Action
             $this->view->form = $form;
             return $this->render('form');
         }
-//         echo "Downloading".$form->code;
+	$values = $form->getValues();
+	$this->view->resultscsv = "<iframe style=\"height:0%;width:0%\" type=\"hidden\" class=\"hidden\" src=\"".
+	 "https://spreadsheets.google.com/tq?tqx=out:csv&tq=select+E,F,G,H,C,J+where+".
+	  "(E+contains+'".$values['autor']."')+and+".
+	  "(F+contains+'".$values['journal']."')+and+".
+	  "(C+contains+'".$values['year']."')+and+".
+	  "(J+contains+'".$values['article']."')+order+by+A+desc&key=0AjqGPI5Q_Ez6dDA3ajhtYVVDOWdBckVhWm1MSFRET1E\"> Downloading</iframe>";
+// 	print_r($value); 
+	echo $this->view->resultscsv;
+        
 	return $this->_forward('search');
+    }
+
+    public function menuAction()
+    {
+        // action body
+	
     }
 
 
 }
+
+
 
 
 
