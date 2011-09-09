735e6b01f16dcafc39a0a62ce96345c70fcc8b27
Sigifredo Escobar Gómez <sigifredo@hell>
2011-09-09 14:31:43 -0500

    agregamos un meta en el layout con la codificación

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -6,10 +6,11 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
    protected function _initView()
    {
       $view = new Zend_View();
-      $view->setEncoding('UTF-8');
-      $view->doctype('HTML5');
+      $view->setEncoding('utf-8');
+      $view->doctype();
       $view->headLink()->appendStylesheet('css/global.css');
-      $viewRenderer = Zend_Controller_Action_HelperBroker::getStaticHelper('ViewRenderer');
+      $view->headScript()->appendFile('js/json.js');
+      $viewRenderer = Zend_Controller_Action_HelperBroker::getStaticHelper('viewRenderer');
       $viewRenderer->setView($view);
       return $view;
    }
