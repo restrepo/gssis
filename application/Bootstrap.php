189923443036fc065445e124880da6b1922d8255
Sigifredo Escobar Gómez <sigifredo@hell>
2011-09-11 15:57:37 -0500

    arreglamos los path de los estilos

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,17 +2,4 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
-   protected function _initView()
-   {
-      $view = new Zend_View();
-      $view->setEncoding('utf-8');
-      $view->doctype();
-      $view->headLink()->appendStylesheet('css/global.css');
-      $view->headScript()->appendFile('js/json.js');
-      $viewRenderer = Zend_Controller_Action_HelperBroker::getStaticHelper('viewRenderer');
-      $viewRenderer->setView($view);
-      return $view;
-   }
 }
-
