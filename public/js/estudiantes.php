4afdf7fe3574e8edbcd120b52e9fc9494a4a147e
Sigifredo Escobar Gómez <sigifredo@hell>
2011-09-20 15:59:39 -0500

    estudiantes - falta fecha y los datos que no están en la hoja de cálculo)

--- public/js/estudiantes.php
+++ public/js/estudiantes.php
@@ -4,7 +4,7 @@ header('content-type: application/x-javascript');
 
 $s = unserialize(urldecode(stripslashes($_GET['s'])));
 
-$query="select J,E,F,I,C";
+$query="select C,D,G where C contains '".$s['name']."' and D contains '".$s['id']."' and G contains '".$s['advisor']."' and H contains '".$s['group']."' and I contains '".$s['projectid']."'";
 
 // if(!empty($s['yearInit']))
 // {
@@ -14,21 +14,23 @@ $query="select J,E,F,I,C";
 // {
 //     $query=$query." and C <= ".$s['yearEnd'];
 // }
+
+
 ?>
 
 var SS_URL = "http://spreadsheets.google.com/tq?key=0AjqGPI5Q_Ez6dHJwV0tzaHRzOXlHcHVZSmwySnRTU1E";
 
 $.ss(SS_URL)
   .setQuery("<?php echo $query; ?>")
-  .setField("J,E,F,I,C")
+  .setField("C,D,G")
   .send(function(success) {
     if(!success) return;
     var con = $('#content')
     var str = "<table class='templateTable'>"
     this.each(function(i, k) {
-      str += "<tr><td colspan='4'>Estudiante matriculado o admitido " + (i+1) + "</td></tr>\
-             <tr><td>Nombre:</td><td>---</td><td>Cédula:</td><td>---</td></tr>\
-             <tr><td>Tutor</td><td>---</td><td>Programa:</td><td>---</td></tr>\
+      str += "<tr><td colspan='4'><b>Estudiante matriculado o admitido " + (i+1) + "</b></td></tr>\
+             <tr><td>Nombre:</td><td>" + this['C'] + "</td><td>Cédula:</td><td>" + this['D'] + "</td></tr>\
+             <tr><td>Tutor:</td><td>" + this['G'] + "</td><td>Programa:</td><td>---</td></tr>\
              <tr><td colspan='2'>Trabajo de investigación o proyecto:</td><td colspan='2'>---</td></tr>"
      })
     str += "</table>"
