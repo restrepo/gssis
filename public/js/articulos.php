81007793a773eda7afd2abaac377b6fd137d3d4b
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2011-10-10 11:41:37 -0500

    agregamos link en articulos para descargar en exel, pero solo mostramos en html

--- public/js/articulos.php
+++ public/js/articulos.php
@@ -5,6 +5,7 @@ header('content-type: application/x-javascript');
 $s = unserialize(urldecode(stripslashes($_GET['s'])));
 
 $query="select J,E,F,I,C where E contains '".$s['autor']."' and F contains '".$s['journal']."' and J contains '".$s['article']."'";
+$query2="select+J,E,F,I,C+where+(E+contains+'".$s['autor']."'+and+F+contains+'".$s['journal']."'+and+J+contains+'".$s['article']."')";
 
 if(!empty($s['yearInit']))
 {
@@ -17,6 +18,7 @@ if(!empty($s['yearEnd']))
 ?>
 
 var SS_URL = "http://spreadsheets.google.com/tq?key=0AjqGPI5Q_Ez6dDA3ajhtYVVDOWdBckVhWm1MSFRET1E";
+var SS_URL_EXEL = "http://spreadsheets.google.com/tq?tqx=out:html&tq=<?php echo $query2; ?>&key=0AjqGPI5Q_Ez6dDA3ajhtYVVDOWdBckVhWm1MSFRET1E";
 
 $.ss(SS_URL)
   .setQuery("<?php echo $query; ?>")
@@ -24,7 +26,7 @@ $.ss(SS_URL)
   .send(function(success) {
     if(!success) return;
     var con = $('#content')
-    var str = "<table class='templateTable'>"
+    var str = "<a href=\"" + SS_URL_EXEL +"\">Exel</a><br><table class='templateTable'>"
     this.each(function(i, k) {
       str += "<tr><td><b>Título del artículo " + (i+1) + "</b></td><td align='center' colspan='2'><b>" + this['J'] + "</b></td></tr>\
              <tr><td>Autores</td><td colspan='2'>" + this['E'] + "</td></tr>\
