3276b0cb3fefcced9293c489b055197e669883f0
Diego Restrepo <rescolo@gmail.com>
2012-10-23 02:10:26 -0500

    Samples of the new dictionaries

---
+++ utilities/journal_alias.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#Don't delete the next line!
+journal_alias={}
+journal_alias['The European Physical Journal C-Particles and Fields']='European Physical Journal C'
+journal_alias['The European Physical Journal B-Condensed Matter and Complex Systems']='European Physical Journal B'
+journal_alias['Phys. Rev., D;(United States)']='Physical Review D'
