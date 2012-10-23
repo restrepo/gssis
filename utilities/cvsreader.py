0c0970e67ea95e349ad10c35ab30b25567f2742f
Diego Restrepo <rescolo@gmail.com>
2012-10-23 02:04:10 -0500

    Log file and issn A log file with dictionary keys with the format: JournalName.Vol.Pag to identify each processed entry have been implemented
    Whenever the log file is presente only the new entries in
    the input csv file is processed.
    
    In addition a first implementation of issn generation is now included.
    The issn is obtained after ordering the output of the publindex
    spreadsheet in journal category in descending order.
    
    A new dictionary with the alias used to name the Journals through
    the csv file was started.

--- utilities/cvsreader.py
+++ utilities/cvsreader.py
@@ -4,11 +4,16 @@
 import csv
 import sys
 import re
+import urllib
+from os import path
 #python database dictionaries
 from authors import *
 from groups import *
 from national import *
 from fullnames import *
+from issn import *
+from newcitationslog import *
+from journal_alias import *
 
 def get_authors_info(dict_ref,dict_authors,dict_groups,dict_fullnames):
     #initalize strings
@@ -41,16 +46,43 @@ def get_authors_info(dict_ref,dict_authors,dict_groups,dict_fullnames):
     auth_institute=re.sub(r'; $','',auth_institute)
     return auth_group,auth_institute
 
+def get_issn(journal):
+    print 'Wating to retrieve issn information for journal: %s' %journal
+    gss_url="http://spreadsheets.google.com"
+    gss_format="csv"
+    gss_query="select B,D where (C contains '%s') order by D asc"\
+                %journal.upper()
+    gss_key="0AjqGPI5Q_Ez6dHV5YWY4MEdFNUs0eW1aeEpoNWJKdEE&gid=0"
+    issn_url="%s/tq?tqx=out:%s&tq=%s&key=%s" %(gss_url,\
+                                               gss_format,\
+                                               gss_query,\
+                                               gss_key)
+
+    issn_file=urllib.urlopen(issn_url)
+    issn_string=issn_file.read()
+    #The value is entry A2 of cvs; 0,1 in python notation:
+    if (issn_string.split('\n')[0]).upper().find('ERROR:')==-1 and issn_string.split('\n')[1]:
+        issn_value=eval(issn_string.split('\n')[1].split(',')[0])
+    else:
+        issn_value='0000-0000'
+
+    issn_file.close()
+    return issn_value
 
 if __name__ == '__main__':
     #generic open
     #f = open(sys.argv[1], 'rt')
     #specific open
+    csvfile='newcitations'
     f = open('citations.csv', 'rt')
-    fw = open('newcitations.csv','w')
+    fw = open('%s.csv' %csvfile,'w')
+    fl = open('%slog.py' %csvfile,'a')
+    fj=open('issn.py','a')
+
+    
     csv_writer=csv.writer(fw)
     csv_writer.writerow(['Año','Tipo','Autor(es)','Revista','Vol.','Pág.','ISSN',\
-                         'Artículo','Impreso','PDF','Group','Tipo','Proyecto ID',\
+                         'Artículo','Impreso','PDF','Group','DOI','Tipo','Proyecto ID',\
                          'Autores UdeA'])
     try:
         reader = csv.DictReader(f)
@@ -62,12 +94,44 @@ if __name__ == '__main__':
             x.append(row)
             typepub='Internacional'
             auth_group,auth_institute=get_authors_info(row,authors,groups,fullnames)
-            #nal o inal
-            if national.has_key(row['Publication']): typepub='Nacional'
-            csv_writer.writerow([row['Year'],typepub,row['Authors'],row['Publication'],\
-                                 row['Volume'],row['Pages'],'',row['Title'],'','',\
-                                 auth_group,'','',auth_institute])
-            print row
+            #check if item already exists
+            logkey=row['Publication'].replace(' ','')+'.'+row['Volume']+'.'+row['Pages']
+            if not entry.has_key(logkey):
+                #Publindex journal Name
+                if journal_alias.has_key(row['Publication']):
+                    row['Publication']=journal_alias[row['Publication']]
+                #nal o inal
+                if national.has_key(row['Publication']): typepub='Nacional'
+                #====  issn information =====
+                journal=row['Publication']
+                #Arxiv has not issn:
+                if journal.upper().find('ARXIV')>=0:
+                    journal='Arxiv'
+                    issn[journal]='0000-0000'
+
+                if not issn.has_key(journal):
+                    issn_value=get_issn(journal)
+                    issn[journal]=issn_value
+                    fj.write("issn['%s']='%s'\n" %(journal,issn_value))
+
+                #===================
+                    
+                        
+                    
+
+
+
+                csv_writer.writerow([row['Year'],typepub,row['Authors'],row['Publication'],\
+                                 row['Volume'],row['Pages'],issn[journal],row['Title'],'','',\
+                                 auth_group,'','','',auth_institute])
+                print row
+                fl.write(r"entry['%s']=True" %logkey)
+                fl.write('\n')
+
+            else:
+                print 'entry:%s, already in %s.csv. Delete entry in %slog.py to uptate' %(logkey,csvfile,csvfile)
     finally:
         f.close()
         fw.close()
+        fl.close()
+        fj.close()
